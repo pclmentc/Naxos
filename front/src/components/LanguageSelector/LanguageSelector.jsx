236b78dac6a1f4509962233bed7247788404826a
pclmentc <pclmentc@gmail.com>
2024-11-07 13:10:37 +0100

    drapeaux

--- front/src/components/LanguageSelector/LanguageSelector.jsx
+++ front/src/components/LanguageSelector/LanguageSelector.jsx
@@ -1,13 +1,15 @@
 import { useLanguage } from '../../context/languageContext';
 import './LanguageSelector.scss';
+import En from '../../assets/langues/en.png';
+import Fr from '../../assets/langues/fr.png';
 
 const LanguageSelector = () => {
   const { switchLanguage } = useLanguage();
 
   return (
     <div className="language-selector">
-      <button onClick={() => switchLanguage('en')}>English</button>
-      <button onClick={() => switchLanguage('fr')}>Français</button>
+      <button onClick={() => switchLanguage('en')}><img src={En} alt="English" /></button>
+      <button onClick={() => switchLanguage('fr')}><img src={Fr} alt="Français" /></button>
     </div>
   );
 };
