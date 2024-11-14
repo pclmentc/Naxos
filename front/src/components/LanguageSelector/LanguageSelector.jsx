e1e3b9fd2458cfe17d2b4b1ba06cbffc6e253c21
pclmentc <pclmentc@gmail.com>
2024-11-14 13:37:46 +0100

    fix hover button

--- front/src/components/LanguageSelector/LanguageSelector.jsx
+++ front/src/components/LanguageSelector/LanguageSelector.jsx
@@ -2,14 +2,31 @@ import { useLanguage } from '../../context/languageContext';
 import './LanguageSelector.scss';
 import En from '../../assets/langues/en.png';
 import Fr from '../../assets/langues/fr.png';
+import { useState } from 'react';
 
 const LanguageSelector = () => {
   const { switchLanguage } = useLanguage();
+  const [activeLanguage, setActiveLanguage] = useState('fr'); // Par défaut 'fr'
+
+  const handleLanguageSwitch = (language) => {
+    setActiveLanguage(language);
+    switchLanguage(language);
+  };
 
   return (
     <div className="language-selector">
-      <button onClick={() => switchLanguage('en')}><img src={En} alt="English" /></button>
-      <button onClick={() => switchLanguage('fr')}><img src={Fr} alt="Français" /></button>
+      <button
+        onClick={() => handleLanguageSwitch('en')}
+        className={activeLanguage === 'en' ? 'active' : ''}
+      >
+        <img src={En} alt="English" />
+      </button>
+      <button
+        onClick={() => handleLanguageSwitch('fr')}
+        className={activeLanguage === 'fr' ? 'active' : ''}
+      >
+        <img src={Fr} alt="Français" />
+      </button>
     </div>
   );
 };
