04f91215e471cc651ca7c1064717ad87d9526329
pclmentc <pclmentc@gmail.com>
2024-11-20 08:54:56 +0100

    feat croisiere

--- front/src/components/Croisieres/Croisieres.jsx
+++ front/src/components/Croisieres/Croisieres.jsx
@@ -1,25 +1,17 @@
-import { useState } from 'react';
 import { useLanguage } from '../../context/languageContext';
-import Slider from 'react-slick';
 import './Croisieres.scss';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 
 const Croisieres = () => {
   const { translations } = useLanguage();
-  const data = translations.croisieres; // Les données pour les croisières
-  const [selectedCruise, setSelectedCruise] = useState(null);
-
-  const openModal = (cruise) => setSelectedCruise(cruise);
-  const closeModal = () => setSelectedCruise(null);
-
-  const sliderSettings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
+  const data = translations.croisieres;
+  const images = [
+    { src: '/images/croisiere1.jpg', alt: 'Les Petites Cyclades' },
+    { src: '/images/croisiere2.jpg', alt: 'Iraklia' },
+    { src: '/images/croisiere3.jpg', alt: 'Delos et Mykonos' },
+    { src: '/images/croisiere4.jpg', alt: 'Mykonos' },
+    { src: '/images/croisiere5.jpg', alt: 'Santorin' },
+    { src: '/images/croisiere6.jpg', alt: 'Paros' }
+  ];
 
   return (
     <section className="croisieres" id="croisieres">
@@ -30,36 +22,17 @@ const Croisieres = () => {
         {data.cruises.map((cruise, index) => (
           <div key={index} className="croisiere-item">
             <h3>{cruise.title}</h3>
-            <Slider {...sliderSettings}>
-              {cruise.images.map((image, idx) => (
-                <img key={idx} src={image} alt={`${cruise.title} ${idx + 1}`} />
-              ))}
-            </Slider>
-            <p>{cruise.description.slice(0, 100)}...</p>
-            <button onClick={() => openModal(cruise)} className="read-more">
-              {translations.readMore}
-            </button>
+            <img
+              src={images[index]?.src}
+              alt={images[index]?.alt}
+              className="croisiere-image"
+            />
+            {cruise.description.split('\n').map((paragraph, idx) => (
+              <p key={idx}>{paragraph}</p>
+            ))}
           </div>
         ))}
       </div>
-
-      {/* Modale responsive */}
-      {selectedCruise && (
-        <div className="modal" onClick={closeModal}>
-          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-            <span className="close" onClick={closeModal}>&times;</span>
-            <h3>{selectedCruise.title}</h3>
-            <Slider {...sliderSettings}>
-              {selectedCruise.images.map((image, idx) => (
-                <img key={idx} src={image} alt={`${selectedCruise.title} ${idx + 1}`} />
-              ))}
-            </Slider>
-            {selectedCruise.description.split('\n').map((line, i) => (
-              <p key={i}>{line}</p>
-            ))}
-          </div>
-        </div>
-      )}
     </section>
   );
 };
