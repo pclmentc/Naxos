cd11b7599c0dc1995711e42510b5381c239b0849
pclmentc <pclmentc@gmail.com>
2024-11-29 10:30:56 +0100

    fix css

--- front/src/components/VideoSection/VideoSection.jsx
+++ front/src/components/VideoSection/VideoSection.jsx
@@ -4,19 +4,20 @@ import { useLanguage } from '../../context/languageContext';
 const VideoSection = () => {
   const { translations } = useLanguage();
   return (
+    <div>
     <div className="video-section">
-
       <article className='videoArticle'>
       <h2>{translations.videoSection}</h2>
-      <p className='citation'>{translations.videoSection1}</p>
       </article>
-
       <video controls>
         <source src="./src/assets/vidéo/IntroNaxos.mp4" type="video/mp4" />
         Votre navigateur ne supporte pas la lecture vidéo.
       </video>
     </div>
-    
+    <div className='photoPort'>
+    <p className='citation'>{translations.videoSection1}</p>
+    </div>
+    </div>
   );
 };
 
