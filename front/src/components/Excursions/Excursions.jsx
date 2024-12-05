213d3102cd41e101337d00d81701f52c0deacc56
pclmentc <pclmentc@gmail.com>
2024-12-05 10:59:26 +0100

    feat img cards excursions privées

--- front/src/components/Excursions/Excursions.jsx
+++ front/src/components/Excursions/Excursions.jsx
@@ -1,14 +1,20 @@
-import { useState, useEffect } from 'react';
-import { useLanguage } from '../../context/languageContext';
-import './Excursions.scss';
+import { useState, useEffect } from "react";
+import { useLanguage } from "../../context/languageContext";
+import "./Excursions.scss";
 
 const Excursions = () => {
   const { translations } = useLanguage();
   const data = translations;
   const [selectedTheme, setSelectedTheme] = useState(null);
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
   const openModal = (theme) => setSelectedTheme(theme);
-  const closeModal = () => setSelectedTheme(null);
+  const closeModal = () => {
+    setSelectedTheme(null);
+    setSelectedCategory(null);
+  };
+
+  const openCategoryModal = (category) => setSelectedCategory(category);
 
   // Gestion de la touche "Escape" pour fermer la modale
   useEffect(() => {
@@ -27,9 +33,9 @@ const Excursions = () => {
           <div
             key={index}
             className="excursion-theme"
-            onClick={() => openModal(theme)} 
-            role="button" 
-            tabIndex="0" 
+            onClick={() => openModal(theme)}
+            role="button"
+            tabIndex="0"
           >
             <img src={theme.image} alt={theme.title} />
             <h3>{theme.title}</h3>
@@ -38,17 +44,65 @@ const Excursions = () => {
         ))}
       </div>
 
-      {/* Modale responsive */}
+      {/* Modale principale */}
       {selectedTheme && (
         <div className="modal" onClick={closeModal}>
           <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-            <span className="close" onClick={closeModal}>&times;</span>
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
             <img src={selectedTheme.image} alt={selectedTheme.title} />
             <h3>{selectedTheme.title}</h3>
-            {selectedTheme.description.split('\n').map((line, i) => (
+            {selectedTheme.description.split("\n").map((line, i) => (
               <p key={i}>{line}</p>
             ))}
-            <img src={selectedTheme.image1} />
+            {selectedTheme.image1 && (
+              <img
+                src={selectedTheme.image1}
+                alt={`${selectedTheme.title} additional`}
+              />
+            )}
+
+            {/* Affichage des catégories si elles existent */}
+            {selectedTheme.categories && (
+              <div className="categories">
+                <h4>Catégories :</h4>
+                <div className="categories-container">
+                  {selectedTheme.categories.map((category, index) => (
+                    <div
+                      key={index}
+                      className="category"
+                      onClick={() => openCategoryModal(category)}
+                      role="button"
+                      tabIndex="0"
+                    >
+                      <h5>{category.title}</h5>
+                      <img src={category.image} alt={category.title} />
+                      <img src={category.image1}  />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+
+      {/* Modale pour les catégories */}
+      {selectedCategory && (
+        <div className="modal" onClick={closeModal}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <img src={selectedCategory.image} alt={selectedCategory.title} />
+            {selectedCategory.image1 && (
+              <img
+                src={selectedCategory.image1}
+                alt={`${selectedCategory.title} additional`}
+              />
+            )}
+            <h4>{selectedCategory.title}</h4>
           </div>
         </div>
       )}
